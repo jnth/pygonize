3dc59a970924535aceee8248c8979551cd3f5b73
Jonathan Virga <jonathan.virga@gmail.com>
2016-06-30 08:12:04 +0200

    Add example.

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pygonize
+print pygonize.__version__
+print pygonize.__file__
+
+# To get logging message...
+import logging
+log = logging.getLogger('pygonize')
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())
+
+# Using the library
+p = pygonize.Pygonize()
+p.read_raster('./test/data/raster.tif')
+p.write_shapefile(range(200, 600, 100), '/tmp/shapefile.shp')
+
+
